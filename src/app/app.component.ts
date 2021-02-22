5830ab1277eaca2b1605f5b36262ce8d8d4d233a
Mariusz Wyszyński <krakus9876@gmail.com>
2021-02-22 17:25:06 +0100

    dodanie nowej zmiennej powitanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pierwsza-nieskomplikowana-aplikacja';
+  powitanie: string = 'pierwsza-nieskomplikowana-aplikacja';
 }
