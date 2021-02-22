07c7436ee7005c22d0a188a080b9f74ad8e43c0a
Mariusz Wyszyński <krakus9876@gmail.com>
2021-02-22 18:29:52 +0100

    dodanie trzeciej zmiennej

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   powitanie: string = 'pierwsza-nieskomplikowana-aplikacja';
   tekstNaDzis: string = 'Głowa do góry'
+  nowaWiadomosc: string = 'Cześć Wszystkim'
 }
